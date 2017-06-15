008d3e0b27051a37f7034d6c3c2ed33e9e940147
spacesh1p <ddagaev1o1@gmail.com>
2017-06-15 19:03:26 +0300

    Port changed..

--- GoodVibes-Server/main.cpp
+++ GoodVibes-Server/main.cpp
@@ -4,17 +4,8 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QString port(argv[1]);
-    qDebug() << port;
-    if (!port.isEmpty()) {
-        int nPort = port.toInt();
-        Server server(nPort);
-        server.start();
-    }
-    else {
-        qDebug() << "You didn't enter the port number.";
-        exit(1);
-    }
+    Server server(2025);
+    server.start();
 
     return a.exec();
 }
